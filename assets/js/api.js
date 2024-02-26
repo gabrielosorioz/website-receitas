eb8f6eb8158d17a32f1b1309a3cb71786bd637d0
gabrielosorioz <gabrielosorio013@gmail.com>
2024-02-25 23:01:25 -0300

    :sparkles: feat(api): rest API that get data from recipe API edamm
    - Setting `ACCESS_POINT`
    - Settings api_key, api_id, type of request
    - function fetch data that recieves an `Array: type [key, value]` and returns the data from  API Json file

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,32 @@
-/**
- * @license MIT
- * @copyright 2023 codewithsadee
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */
+
 
 "use strict";
+
+/**
+ * @param {Array} queries Query array
+ * @param {Function} successCallback Success callback function
+ */
+
+
+window.ACCESS_POINT = "https://api.edamam.com/api/recipes/v2";
+
+const /** {String} */ APP_ID = "3c5e4494";
+const /** {String} */ API_KEY = "2d54d12fdae6ebded76996ff8f158826";
+const /** {String} */ TYPE = "public";
+
+export const fetchData = async function (queries, successCallback) {
+    const /** {String} */ query = queries?.join("&")
+        .replace(/,/g,"=")
+        .replace(/ /g,"%20")
+        .replace(/\+/g,"%2B");
+    
+    const /** {String} */ url = `${ACCESS_POINT}?type=${TYPE}&app_id=${APP_ID}&app_key=${API_KEY}
+    ${query ? `&${query}` : ""}`;
+                     
+    const /** {Object} */ response = await fetch(url);
+
+    if(response.ok){
+        const data = await response.json();
+        successCallback(data);
+    }
+}
