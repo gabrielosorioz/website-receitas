caf9da421b25d482fcd6b89c93910683ec136512
gabrielosorioz <gabrielosorio013@gmail.com>
2024-03-01 17:40:32 -0300

    :sparkles:feat: get data for tab content
    - function `addTabContent` that create a grid list with products from recipe API edamm

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,6 +1,13 @@
 
 
 "use strict";
+/**
+ * Import
+ */
+
+import {fetchData} from "./api.js";
+import { $skeletonCard, cardQueries } from "./global.js"
+import { getTime } from "./module.js";
 
 
 /**
@@ -30,11 +37,9 @@ $searchField.addEventListener("keydown", e => {
  * Tab panel navigation
  */
 
-const /** {NodeList} */ $tabBtns = document.querySelectorAll(
-    "[data-tab-btn]");
+const /** {NodeList} */ $tabBtns = document.querySelectorAll("[data-tab-btn]");
     
-const /** {NodeList} */ $tabPanels = document.querySelectorAll(
-    "[data-tab-panel]");
+const /** {NodeList} */ $tabPanels = document.querySelectorAll("[data-tab-panel]");
 
 let /** NodeElement */ [$lastActiveTabPanel] = $tabPanels;
 let /** NodeElement */ [$lastActiveTabBtn] = $tabBtns;
@@ -45,14 +50,15 @@ addEventOnElements($tabBtns,"click", function (){
     $lastActiveTabBtn.setAttribute("tabindex", -1);
 
 
-    const /**{NodeElement} */ $currentTabPanel = document.querySelector(`
-    #${this.getAttribute("aria-controls")}`);
+    const /**{NodeElement} */ $currentTabPanel = document.querySelector(`#${this.getAttribute("aria-controls")}`);
     $currentTabPanel.removeAttribute("hidden");
     this.setAttribute("aria-selected", true);
     this.setAttribute("tabindex",0);
 
     $lastActiveTabPanel = $currentTabPanel;
     $lastActiveTabBtn = this;
+
+    addTabContent(this, $currentTabPanel);
 })
 
 /**
@@ -81,3 +87,93 @@ addEventOnElements($tabBtns, "keydown", function (e) {
     }
 
 });
+
+/**
+ * WORK WITH API
+ * fetch data for tab content
+ */
+
+const addTabContent = ($currentTabBtn, $currentTabPanel) => {
+    const /** {NodeElement} */ $gridList = document.createElement("div");
+    $gridList.classList.add("grid-list");
+
+    $currentTabPanel.innerHTML = `
+        <div class="grid-list"> 
+            ${$skeletonCard.repeat(12)}
+        </div>
+    
+    `;
+
+    fetchData([['mealType', $currentTabBtn.textContent.trim().toLowerCase()],
+    ...cardQueries], function (data) {
+
+        $currentTabPanel.innerHTML = "";
+
+        for (let i = 0; i < 12; i++) {
+
+            const { 
+               recipe: {
+                image,
+                label:title,
+                totalTime: cookingTime,
+                uri
+               } 
+            } = data.hits[i];
+
+            const /** {String} */ recipeId = uri.slice(uri.lastIndexOf("_") + 1);
+            const /** {Undefined || String} */ isSaved = window.localStorage.getItem(`cookio-recipe${recipeId}`); 
+
+            const /** {NodeElement} */ $card = document.createElement("div");
+            $card.classList.add("card");
+            $card.style.animationDelay = `${100 * i}ms`;
+
+            $card.innerHTML = `
+            
+            <figure class="card-media img-holder">
+            <img src="${image}" alt="${title}"
+            class="img-cover" width="195" height="195" loading="lazy">
+            </figure>
+
+            <div class="card-body">
+                <h3 class="title-small">
+                    <a href="./detail.html?recipe=${recipeId}" class="card-link">${title ?? "Untitled"}</a>
+                </h3>
+
+                <div class="meta-wrapper">
+                    <div class="meta-item">
+                        <span class="material-symbols-outlined" 
+                        aria-hidden="true">schedule</span>
+
+                        <span class="label-medium">${getTime(cookingTime).time || "<1"}
+                         ${getTime(cookingTime).timeUnit}</span>
+
+                    </div>
+
+                    <button class="icon-btn has-state ${isSaved ? "saved" : "removed"}" 
+                    aria-label="Add to saved recipes" onClick="saveRecipe(this, '${recipeId}')">
+                        <span class="material-symbols-outlined bookmark-add" 
+                        aria-hidden="true">bookmark_add</span>
+
+                        <span class="material-symbols-outlined bookmark"
+                        aria-hidden="true">bookmark</span>
+                    </button>
+                </div>
+            </div>
+            
+            
+            
+            `;
+
+            $gridList.appendChild($card);
+            
+        }
+
+        $currentTabPanel.appendChild($gridList);
+
+        $currentTabPanel.innerHTML += `
+            <a href="./recipes.html?mealtype=${$currentTabBtn.textContent.trim().toLowerCase()}" 
+            class="btn btn-secondary label-large has-state">Show more</a>
+            `
+
+    })
+}
